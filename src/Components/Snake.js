89a153b57ca92cde8a5b1c249b8e1a73f340b90e
Sankalp Abhale <55441469+abhalesankalp@users.noreply.github.com>
2020-05-13 17:48:22 +0530

    snake created

--- src/Components/Snake.js
+++ src/Components/Snake.js
@@ -1,7 +1,37 @@
 import React from "react";
+import "animate.css/animate.css";
 class Snake extends React.Component {
+  moveCircle = () => {
+    setInterval(function() {
+      let leftDim = this.state.MyStyle.left;
+      let leftVal = parseInt(leftDim.substring(0, 2), 3);
+      let myStyleCopy = JSON.parse(JSON.stringify(this.state.MyStyle));
+      myStyleCopy.left = leftVal + 10 + "px";
+      this.setState({ recipes: myStyleCopy });
+    }, 3000);
+  };
+
+  componentDidMount = () => {
+    this.moveCircle();
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      MyStyle: {
+        left: "10px",
+        top: "10px",
+        width: "12px",
+        height: "12px",
+        "background-color": "yellow",
+        border: "1px ridge red",
+        "border-radius": "40%",
+        position: "relative"
+      }
+    };
+  }
   render() {
-    return <div id="SnakeDiv" />;
+    return <div className="SnakeDiv" style={this.state.MyStyle} />;
   }
 }
 export default Snake;
--- src/Components/SnakeGameBorad.js
+++ src/Components/SnakeGameBorad.js
@@ -1,7 +1,15 @@
 import React from "react";
+import SnakeDiv from "./Snake";
 class SnakeGameBoard extends React.Component {
   render() {
-    return <div className="GameBoard" />;
+    return (
+      <div className="GameBoard">
+        <SnakeDiv />
+        <button className="ButtonCSS" name="StartGame" title="Start Game">
+          "Start Game"
+        </button>
+      </div>
+    );
   }
 }
 
